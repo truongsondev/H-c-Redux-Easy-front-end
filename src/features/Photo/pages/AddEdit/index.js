b9e7a719556ecad9d98ae9aef0cb88cf66fb3488
FIRST_NAME LAST_NAME <truongsonpt.80@gmail.com>
2022-07-22 16:00:45 +0700

    Cập nhật Thay đổi lần 1

---
+++ src/features/Photo/pages/AddEdit/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+AddEditPage.propTypes = {};
+
+function AddEditPage(props) {
+  return <div>Photo Add Page edit</div>;
+}
+
+export default AddEditPage;
---
+++ src/features/Photo/pages/Main/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+MainPage.propTypes = {};
+
+function MainPage(props) {
+  return <div>Photo Main Page asdas</div>;
+}
+
+export default MainPage;
